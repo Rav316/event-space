54aa015c7318f99ba66f2eed061f9bf0f793a192
Alex <manyakinsasha@gmail.com>
2025-09-25 19:48:54 +0300

    change password validation schema

--- event-space-app/src/schemas/password-create-schema.ts
+++ event-space-app/src/schemas/password-create-schema.ts
@@ -1,17 +1,19 @@
 import { z } from 'zod';
 
-export const passwordCreateSchema = z.object({
-  password: z
-    .string()
-    .min(4, { message: 'Пароль должен содержать не менее 4 символов' })
-    .regex(/^[a-zA-Z0-9@#$%^&+=]*$/, {
-      message:
-        'Пароль должен содержать только буквы латинского алфавита, цифры и специальные символы',
-    }),
-  confirmPassword: z.string()
-}).refine(data => data.password === data.confirmPassword, {
-  path: ['confirmPassword'],
-  message: 'Пароли не совпадают',
-});
+export const passwordCreateSchema = z
+  .object({
+    password: z
+      .string()
+      .min(4, { message: 'Пароль должен содержать не менее 4 символов' })
+      .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/, {
+        message:
+          'Пароль должен содержать только буквы латинского алфавита, цифры и специальные символы',
+      }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ['confirmPassword'],
+    message: 'Пароли не совпадают',
+  });
 
 export type PasswordCreateData = z.infer<typeof passwordCreateSchema>;
