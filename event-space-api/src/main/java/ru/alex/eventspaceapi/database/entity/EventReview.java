f0f58d362229ea5ff813d27d5d67199a44001464
Alex <manyakinsasha@gmail.com>
2025-10-17 21:37:38 +0300

    add the event_review table and entity

---
+++ event-space-api/src/main/java/ru/alex/eventspaceapi/database/entity/EventReview.java
@@ -0,0 +1,36 @@
+package ru.alex.eventspaceapi.database.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "event_review")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "event_id", referencedColumnName = "id")
+    private Event event;
+
+    private String title;
+    private String content;
+    private Short rating;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+}
