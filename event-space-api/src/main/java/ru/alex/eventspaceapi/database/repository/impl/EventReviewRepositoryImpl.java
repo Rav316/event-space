370d901f3a3750e8055d69e17ad2f6dd36380859
Alex <manyakinsasha@gmail.com>
2025-10-19 14:30:27 +0300

    add filtering and sorting for event reviews request

--- event-space-api/src/main/java/ru/alex/eventspaceapi/database/repository/impl/EventReviewRepositoryImpl.java
+++ event-space-api/src/main/java/ru/alex/eventspaceapi/database/repository/impl/EventReviewRepositoryImpl.java
@@ -1,5 +1,8 @@
 package ru.alex.eventspaceapi.database.repository.impl;
 
+import com.querydsl.core.types.OrderSpecifier;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -14,6 +17,7 @@ import ru.alex.eventspaceapi.dto.eventReview.EventReviewStatisticsDto;
 import ru.alex.eventspaceapi.dto.filter.EventReviewFilter;
 import ru.alex.eventspaceapi.mapper.eventReview.EventReviewStatisticsMapper;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static ru.alex.eventspaceapi.database.entity.QEventReview.eventReview;
@@ -34,7 +38,8 @@ public class EventReviewRepositoryImpl implements EventReviewRepositoryCustom {
         List<EventReview> reviews = queryFactory
                 .selectFrom(eventReview)
                 .leftJoin(eventReview.author, user).fetchJoin()
-                .where(eventReview.event.id.eq(eventId))
+                .where(eventReview.event.id.eq(eventId).and(buildPredicate(filter)))
+                .orderBy(getSortOrder(filter))
                 .offset((long) page * pageSize)
                 .limit(pageSize + 1)
                 .fetch();
@@ -65,5 +70,36 @@ public class EventReviewRepositoryImpl implements EventReviewRepositoryCustom {
                 .addValue("eventId", eventId);
 
         return jdbcTemplate.queryForObject(sql, params, eventReviewStatisticsMapper);
+    }
+
+    private BooleanExpression buildPredicate(EventReviewFilter filter) {
+        BooleanExpression predicate = Expressions.TRUE.isTrue();
+        if(filter.rating() != null) {
+            predicate = predicate.and(eventReview.rating.eq(filter.rating()));
+        }
+        return predicate;
+    }
+
+    private OrderSpecifier<?>[] getSortOrder(EventReviewFilter filter) {
+        List<OrderSpecifier<?>> orderSpecifiers = new ArrayList<>();
+
+        if(filter.sort() != null) {
+            switch (filter.sort()) {
+                case "new":
+                    orderSpecifiers.add(eventReview.createdAt.desc());
+                    break;
+                case "old":
+                    orderSpecifiers.add(eventReview.createdAt.asc());
+                    break;
+                case "high-rating":
+                    orderSpecifiers.add(eventReview.rating.desc());
+                    break;
+                case "low-rating":
+                    orderSpecifiers.add(eventReview.rating.asc());
+                    break;
+            }
+        }
+        orderSpecifiers.add(eventReview.id.desc());
+        return orderSpecifiers.toArray(new OrderSpecifier[0]);
     }
 }
