1675576ca544dbbb236ad00e5692b35ac7abf5bb
Alex <manyakinsasha@gmail.com>
2025-10-22 20:46:40 +0300

    add openAPI/swagger-ui in project

---
+++ event-space-api/src/main/java/ru/alex/eventspaceapi/config/OpenApiConfig.java
@@ -0,0 +1,35 @@
+package ru.alex.eventspaceapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI caseOpenAPI() {
+        String schemeName = "bearerAuth";
+        String bearerFormat = "JWT";
+        String scheme = "bearer";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(schemeName)).components(new Components()
+                        .addSecuritySchemes(
+                                schemeName, new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .bearerFormat(bearerFormat)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .scheme(scheme)
+                        )
+                )
+                .info(new Info()
+                        .title("Event Space API")
+                );
+    }
+}
+
