70c33a6b8e39e10ee71fd78d2860893e90dd8624
Alex <manyakinsasha@gmail.com>
2025-11-08 09:45:27 +0300

    Add responsive layout to the "My Reviews" tab on the statistics page

--- event-space-app/src/components/shared/event-review/review-stat-item.tsx
+++ event-space-app/src/components/shared/event-review/review-stat-item.tsx
@@ -41,29 +41,37 @@ export const ReviewStatItem: React.FC<Props> = ({
         <span className={'font-medium'}>{name}</span>
         <StarRating rating={rating} starSize={15} className={'gap-1'} />
       </div>
-      <div className={'flex items-center gap-2'}>
+      <div className={'flex items-center gap-2 max-[475px]:flex-col max-[475px]:items-start'}>
         <Badge variant={'outline'} className={categoryColors[category.id - 1]}>
           {category.name}
         </Badge>
         <div className={'flex items-center gap-2'}>
-          <Calendar className={'text-muted-foreground'} size={15} />
-          <span className={'text-muted-foreground text-sm'}>
+          <div className={'flex items-center gap-2'}>
+            <Calendar className={'text-muted-foreground'} size={15} />
+            <span className={'text-muted-foreground text-sm'}>
             {formatDateToRuFormat(date)}
           </span>
-        </div>
-        <div className={'flex items-center gap-2'}>
-          <Users className={'text-muted-foreground'} size={15} />
-          <span className={'text-muted-foreground text-sm'}>
+          </div>
+          <div className={'flex items-center gap-2'}>
+            <Users className={'text-muted-foreground'} size={15} />
+            <span className={'text-muted-foreground text-sm'}>
             {participantQuantity} участников
           </span>
+          </div>
         </div>
       </div>
-      <p className={'max-[410px]:leading-5'}>{content.slice(0, 225) + '…'}</p>
-      <div className={'flex items-center gap-2 justify-between'}>
+      <p className={'max-[410px]:leading-5 break-words'}>
+        {content.length > 225 ? content.slice(0, 225) + '…' : content}
+      </p>
+      <div className={'flex items-center gap-2 justify-between max-[500px]:flex-col max-[500px]:items-start'}>
         <span className={'text-muted-foreground text-sm'}>
           {timeAgo(createdAt)}
         </span>
-        <Button variant={'ghost'} onClick={() => navigate(`/events/${eventId}`)}>
+        <Button
+          className={'max-[500px]:w-full'}
+          variant={'ghost'}
+          onClick={() => navigate(`/events/${eventId}`)}
+        >
           <span className={'text-sm'}>Перейти к событию</span>
           <ExternalLink size={13} />
         </Button>
--- event-space-app/src/components/shared/event-statistics/my-reviews-tab.tsx
+++ event-space-app/src/components/shared/event-statistics/my-reviews-tab.tsx
@@ -26,36 +26,44 @@ export const MyReviewsTab = () => {
   }
 
   return (
-    <div className={'flex flex-col gap-5 w-full'}>
-      <div className={'flex items-center gap-5 w-full'}>
-        <Card className={'w-full h-full'}>
-          <CardHeader>
-            <CardTitle className={'flex items-center gap-2'}>
-              <Divide />
-              Моя средняя оценка
-            </CardTitle>
-          </CardHeader>
-          <CardContent className={'h-full flex items-center justify-center'}>
-            <div className="flex flex-col gap-2 w-fit items-center">
-              <h3 className="font-medium text-4xl">{statistics.avgRating.toFixed(2)}</h3>
-              <StarRating rating={statistics.avgRating} className={'gap-2'} />
-              <span className={'text-muted-foreground'}>
+    <div className='flex flex-col gap-5 w-full'>
+      <div className='flex items-stretch gap-5 w-full max-[1000px]:flex-col'>
+        <div className='flex flex-2 gap-5 max-[625px]:flex-col'>
+          <Card className='w-full h-full'>
+            <CardHeader>
+              <CardTitle className='flex items-center gap-2'>
+                <Divide />
+                Моя средняя оценка
+              </CardTitle>
+            </CardHeader>
+            <CardContent className='h-full flex items-center justify-center'>
+              <div className='flex flex-col gap-2 w-fit items-center'>
+                <h3 className='font-medium text-4xl'>{statistics.avgRating.toFixed(2)}</h3>
+                <StarRating rating={statistics.avgRating} className='gap-2' />
+                <span className='text-muted-foreground'>
                 из {statistics.total} отзывов
               </span>
-            </div>
-          </CardContent>
-        </Card>
-        <RatingDistributionBlock
-          total={statistics.total}
-          fiveStars={statistics.fiveStars}
-          fourStars={statistics.fourStars}
-          threeStars={statistics.threeStars}
-          twoStars={statistics.twoStars}
-          oneStar={statistics.oneStar}
-        />
-        <TopWordsBlock words={words} />
+              </div>
+            </CardContent>
+          </Card>
+
+          <TopWordsBlock words={words} />
+        </div>
+
+        <div className='flex-1'>
+          <RatingDistributionBlock
+            total={statistics.total}
+            fiveStars={statistics.fiveStars}
+            fourStars={statistics.fourStars}
+            threeStars={statistics.threeStars}
+            twoStars={statistics.twoStars}
+            oneStar={statistics.oneStar}
+          />
+        </div>
       </div>
+
       <LastReviewsList />
     </div>
   );
+
 };
