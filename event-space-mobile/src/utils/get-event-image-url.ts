9040e84ba2a83b8cabc0144d2f9ace5ca8d575f8
Alex <manyakinsasha@gmail.com>
2026-04-29 09:36:49 +0300

    small fixes

--- event-space-mobile/src/utils/get-event-image-url.ts
+++ event-space-mobile/src/utils/get-event-image-url.ts
@@ -1,5 +1,5 @@
 export const getEventImageUrl = (imageUrl: string) => {
   const staticUrl = process.env.EXPO_PUBLIC_STATIC_URL;
-  return imageUrl
-    ? `${staticUrl}${imageUrl}` : undefined
+  if (!imageUrl) return undefined;
+  return /^https?:\/\//i.test(imageUrl) ? imageUrl : `${staticUrl}${imageUrl}`;
 };
